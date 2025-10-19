bd1207c8ece8dd44215d424201c8c265fd9483e8
Biquaternions <biquaternions@serlith.net>
2025-10-19 12:50:07 -0500

    Add header base

--- app/components/profiler-header.vue
+++ app/components/profiler-header.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
 
+import {faDiscord, faGithub} from "@fortawesome/free-brands-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+
+const appConfig = useAppConfig();
+const route = useRoute();
+
 </script>
 
 <template>
+<header class="border-b" style="background-color: var(--background-color); border-color: var(--border-color);">
+  <div class="flex flex-col sm:flex-row sm:items-center sm:justify-between p-4 sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl mx-auto">
+    <div class="flex flex-row gap-2 text-pink-300 sm:mb-0 text-2xl font-bold">
+      <img src="~/assets/airplane.png" alt="airplane" width="60">
+      <div>{{ appConfig.title }}</div>
+    </div>
 
+    <div v-if="route.path !== '/'" class="flex items-center justify-center text-2xl space-x-6 text-white">
+      <!-- Add buttons -->
+    </div>
+  </div>
+</header>
 </template>
 
 <style scoped>
