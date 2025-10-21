8f4b8e3cb13c5c9ea395f29f5c4b1066437d4bd4
Biquaternions <biquaternions@serlith.net>
2025-10-20 22:31:36 -0500

    Fix animations

--- app/components/system/card.vue
+++ app/components/system/card.vue
@@ -23,113 +23,115 @@ const flags = flare.profile.vmOptions.flags.join(" ")
 
 <template>
   <div class="flex flex-col items-center min-h-6 min-w-[95%] m-8 max-w-sm p-6 gap-4">
-    <div class="flex flex-wrap items-start justify-center min-w-full">
-      <div class="min-h-64 min-w-[40%] m-8 max-w-sm p-6 gap-4 rounded-lg shadow-sm" style="background-color: var(--surface-color)">
-        <div class="flex flex-row gap-4 items-center text-2xl font-bold">
-          <FontAwesomeIcon :icon="faGears" class="text-pink-400" />
-          <span>Hardware</span>
-        </div>
-        <div class="flex flex-col gap-1 items-start mt-4">
-          <div class="flex flex-row items-center gap-2">
-            <span class="text-lg font-bold">CPU Brand:</span>
-            <span class="text-gray-400">{{ flare.profile.hwInfo.cpu.model }}</span>
+    <div>
+      <div class="flex flex-wrap items-start justify-center min-w-full">
+        <div class="min-h-64 min-w-[40%] m-8 max-w-sm p-6 gap-4 rounded-lg shadow-sm" style="background-color: var(--surface-color)">
+          <div class="flex flex-row gap-4 items-center text-2xl font-bold">
+            <FontAwesomeIcon :icon="faGears" class="text-pink-400" />
+            <span>Hardware</span>
           </div>
-          <div class="flex flex-row items-center gap-2">
-            <span class="text-lg font-bold">CPU Cores:</span>
-            <span class="text-gray-400">{{ flare.profile.hwInfo.cpu.coreCount }} ({{ flare.profile.hwInfo.cpu.threadCount }} threads)</span>
-          </div>
-          <div class="flex flex-row items-center gap-2">
-            <span class="text-lg font-bold">RAM Total:</span>
-            <span class="text-gray-400">{{ Math.round(flare.profile.hwInfo.memory.total / 10737418.24) / 100 }} GiB</span>
-          </div>
-          <div class="flex flex-row items-center gap-2">
-            <span class="text-lg font-bold">SWAP Total:</span>
-            <span class="text-gray-400">{{ Math.round(flare.profile.hwInfo.memory.swapTotal / 10737418.24) / 100 }} GiB</span>
-          </div>
-          <div class="flex flex-row items-center gap-2">
-            <span class="text-lg font-bold">Virtual Max:</span>
-            <span class="text-gray-400">{{ Math.round(flare.profile.hwInfo.memory.virtualMax / 10737418.24) / 100 }} GiB</span>
-          </div>
-        </div>
-      </div>
-      <div class="min-h-64 min-w-[40%] m-8 max-w-sm p-6 gap-4 rounded-lg shadow-sm" style="background-color: var(--surface-color)">
-        <div class="flex flex-row gap-4 items-center text-2xl font-bold">
-          <FontAwesomeIcon :icon="faDesktop" class="text-pink-400" />
-          <span>Operating System</span>
-        </div>
-        <div class="flex flex-col gap-1 items-start mt-4">
-          <div class="flex flex-row items-center gap-2">
-            <span class="text-lg font-bold">OS:</span>
-            <span class="text-gray-400">{{ flare.profile.os.family }}</span>
-          </div>
-          <div class="flex flex-row items-center gap-2">
-            <span class="text-lg font-bold">Version:</span>
-            <span class="text-gray-400">{{ flare.profile.os.version }}</span>
-          </div>
-          <div class="flex flex-row items-center gap-2">
-            <span class="text-lg font-bold">Family:</span>
-            <span class="text-gray-400">{{ flare.profile.os.manufacturer }}</span>
-          </div>
-          <div class="flex flex-row items-center gap-2">
-            <span class="text-lg font-bold">Architecture:</span>
-            <span class="text-gray-400">{{ flare.profile.os.bitness }} bits</span>
+          <div class="flex flex-col gap-1 items-start mt-4">
+            <div class="flex flex-row items-center gap-2">
+              <span class="text-lg font-bold">CPU Brand:</span>
+              <span class="text-gray-400">{{ flare.profile.hwInfo.cpu.model }}</span>
+            </div>
+            <div class="flex flex-row items-center gap-2">
+              <span class="text-lg font-bold">CPU Cores:</span>
+              <span class="text-gray-400">{{ flare.profile.hwInfo.cpu.coreCount }} ({{ flare.profile.hwInfo.cpu.threadCount }} threads)</span>
+            </div>
+            <div class="flex flex-row items-center gap-2">
+              <span class="text-lg font-bold">RAM Total:</span>
+              <span class="text-gray-400">{{ Math.round(flare.profile.hwInfo.memory.total / 10737418.24) / 100 }} GiB</span>
+            </div>
+            <div class="flex flex-row items-center gap-2">
+              <span class="text-lg font-bold">SWAP Total:</span>
+              <span class="text-gray-400">{{ Math.round(flare.profile.hwInfo.memory.swapTotal / 10737418.24) / 100 }} GiB</span>
+            </div>
+            <div class="flex flex-row items-center gap-2">
+              <span class="text-lg font-bold">Virtual Max:</span>
+              <span class="text-gray-400">{{ Math.round(flare.profile.hwInfo.memory.virtualMax / 10737418.24) / 100 }} GiB</span>
+            </div>
           </div>
         </div>
-      </div>
-      <div class="min-h-64 min-w-[40%] m-8 max-w-sm p-6 gap-4 rounded-lg shadow-sm" style="background-color: var(--surface-color)">
-        <div class="flex flex-row gap-4 items-center text-2xl font-bold">
-          <FontAwesomeIcon :icon="faMugHot" class="text-pink-400" />
-          <span>Java</span>
+        <div class="min-h-64 min-w-[40%] m-8 max-w-sm p-6 gap-4 rounded-lg shadow-sm" style="background-color: var(--surface-color)">
+          <div class="flex flex-row gap-4 items-center text-2xl font-bold">
+            <FontAwesomeIcon :icon="faDesktop" class="text-pink-400" />
+            <span>Operating System</span>
+          </div>
+          <div class="flex flex-col gap-1 items-start mt-4">
+            <div class="flex flex-row items-center gap-2">
+              <span class="text-lg font-bold">OS:</span>
+              <span class="text-gray-400">{{ flare.profile.os.family }}</span>
+            </div>
+            <div class="flex flex-row items-center gap-2">
+              <span class="text-lg font-bold">Version:</span>
+              <span class="text-gray-400">{{ flare.profile.os.version }}</span>
+            </div>
+            <div class="flex flex-row items-center gap-2">
+              <span class="text-lg font-bold">Family:</span>
+              <span class="text-gray-400">{{ flare.profile.os.manufacturer }}</span>
+            </div>
+            <div class="flex flex-row items-center gap-2">
+              <span class="text-lg font-bold">Architecture:</span>
+              <span class="text-gray-400">{{ flare.profile.os.bitness }} bits</span>
+            </div>
+          </div>
         </div>
-        <div class="flex flex-col gap-1 items-start mt-4">
-          <div class="flex flex-row items-center gap-2">
-            <span class="text-lg font-bold">Vendor:</span>
-            <span class="text-gray-400">{{ flare.profile.vmOptions.vendor }}</span>
+        <div class="min-h-64 min-w-[40%] m-8 max-w-sm p-6 gap-4 rounded-lg shadow-sm" style="background-color: var(--surface-color)">
+          <div class="flex flex-row gap-4 items-center text-2xl font-bold">
+            <FontAwesomeIcon :icon="faMugHot" class="text-pink-400" />
+            <span>Java</span>
           </div>
-          <div class="flex flex-row items-center gap-2">
-            <span class="text-lg font-bold">Version:</span>
-            <span class="text-gray-400">{{ flare.profile.vmOptions.version }}</span>
-          </div>
-          <div class="flex flex-row items-center gap-2">
-            <span class="text-lg font-bold">Runtime:</span>
-            <span class="text-gray-400">{{ flare.profile.vmOptions.runtimeName }} {{ flare.profile.vmOptions.runtimeVersion }}</span>
-          </div>
-          <div class="flex flex-row items-center gap-2">
-            <span class="text-lg font-bold">JVM:</span>
-            <span class="text-gray-400">{{ flare.profile.vmOptions.vm }}</span>
+          <div class="flex flex-col gap-1 items-start mt-4">
+            <div class="flex flex-row items-center gap-2">
+              <span class="text-lg font-bold">Vendor:</span>
+              <span class="text-gray-400">{{ flare.profile.vmOptions.vendor }}</span>
+            </div>
+            <div class="flex flex-row items-center gap-2">
+              <span class="text-lg font-bold">Version:</span>
+              <span class="text-gray-400">{{ flare.profile.vmOptions.version }}</span>
+            </div>
+            <div class="flex flex-row items-center gap-2">
+              <span class="text-lg font-bold">Runtime:</span>
+              <span class="text-gray-400">{{ flare.profile.vmOptions.runtimeName }} {{ flare.profile.vmOptions.runtimeVersion }}</span>
+            </div>
+            <div class="flex flex-row items-center gap-2">
+              <span class="text-lg font-bold">JVM:</span>
+              <span class="text-gray-400">{{ flare.profile.vmOptions.vm }}</span>
+            </div>
           </div>
         </div>
-      </div>
-      <div class="min-h-64 min-w-[40%] m-8 max-w-sm p-6 gap-4 rounded-lg shadow-sm" style="background-color: var(--surface-color)">
-        <div class="flex flex-row gap-4 items-center text-2xl font-bold">
-          <FontAwesomeIcon :icon="faServer" class="text-pink-400" />
-          <span>Server</span>
-        </div>
-        <div class="flex flex-col gap-1 items-start mt-4">
-          <div class="flex flex-row items-center gap-2">
-            <span class="text-lg font-bold">Build:</span>
-            <span class="text-gray-400">{{ flare.profile.v3.versions["Primary Version"] }}</span>
+        <div class="min-h-64 min-w-[40%] m-8 max-w-sm p-6 gap-4 rounded-lg shadow-sm" style="background-color: var(--surface-color)">
+          <div class="flex flex-row gap-4 items-center text-2xl font-bold">
+            <FontAwesomeIcon :icon="faServer" class="text-pink-400" />
+            <span>Server</span>
           </div>
-          <div class="flex flex-row items-center gap-2">
-            <span class="text-lg font-bold">API:</span>
-            <span class="text-gray-400">{{ flare.profile.v3.versions["Bukkit Version"] }}</span>
-          </div>
-          <div class="flex flex-row items-center gap-2">
-            <span class="text-lg font-bold">Version:</span>
-            <span class="text-gray-400">{{ flare.profile.v3.versions["Minecraft Version"] }}</span>
+          <div class="flex flex-col gap-1 items-start mt-4">
+            <div class="flex flex-row items-center gap-2">
+              <span class="text-lg font-bold">Build:</span>
+              <span class="text-gray-400">{{ flare.profile.v3.versions["Primary Version"] }}</span>
+            </div>
+            <div class="flex flex-row items-center gap-2">
+              <span class="text-lg font-bold">API:</span>
+              <span class="text-gray-400">{{ flare.profile.v3.versions["Bukkit Version"] }}</span>
+            </div>
+            <div class="flex flex-row items-center gap-2">
+              <span class="text-lg font-bold">Version:</span>
+              <span class="text-gray-400">{{ flare.profile.v3.versions["Minecraft Version"] }}</span>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-  <div class="flex flex-col items-start min-h-6 min-w-[75%] mx-8 mb-16 max-w-sm p-6 gap-6 rounded-lg shadow-sm" style="background-color: var(--surface-color)">
-    <div class="flex flex-row gap-4 items-center text-2xl font-bold">
-      <FontAwesomeIcon :icon="faTerminal" class="text-pink-400" />
-      <span>Flags</span>
-    </div>
-    <span class="text-gray-400">
+    <div class="flex flex-col items-start min-h-6 min-w-[75%] mx-8 mb-16 max-w-sm p-6 gap-6 rounded-lg shadow-sm" style="background-color: var(--surface-color)">
+      <div class="flex flex-row gap-4 items-center text-2xl font-bold">
+        <FontAwesomeIcon :icon="faTerminal" class="text-pink-400" />
+        <span>Flags</span>
+      </div>
+      <span class="text-gray-400">
       {{ flags }}
     </span>
+    </div>
   </div>
 </template>
 
