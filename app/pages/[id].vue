a6bc459c12f0a2f673764fb982f54335f7c1b97e
Biquaternions <biquaternions@serlith.net>
2025-10-22 22:45:16 -0500

    Decompress data and timeline

--- app/pages/[id].vue
+++ app/pages/[id].vue
@@ -2,7 +2,7 @@
 
 import {useProfilerScreenStore} from "~/store/profiler-screen-store";
 import type {FlareProfile} from "~/types/profiler";
-import {CreateProfile} from "~/proto/ProfileFile_pb";
+import {CreateProfile, AirplaneProfileFile, TimelineFile} from "~/proto/ProfileFile_pb";
 import {b64UnzipBytes} from "~/util/binary-utils";
 
 const route = useRoute()
@@ -10,29 +10,35 @@ const id = route.params.id
 
 const config = useRuntimeConfig()
 const screenStore = useProfilerScreenStore()
-const { error, pending, data } = await useFetch<FlareProfile>(`${config.public.apiBackendUrl}/api/profiler/${id}`)
+const { error, data } = await useFetch<FlareProfile>(`${config.public.apiBackendUrl}/api/profiler/${id}`)
 
+const status = ref<"pending" | "ready">("pending")
 const profile = ref<CreateProfile | null>(null)
-watch(data, (dataReady) => { // Maybe use onMounted ?
+const dataSamples = ref<AirplaneProfileFile[] | null>(null)
+const timelineSamples = ref<TimelineFile[] | null>(null)
+watch(data, (dataReady) => {
   if (dataReady) {
     profile.value = CreateProfile.fromBinary(b64UnzipBytes(dataReady.raw));
+    dataSamples.value = dataReady.dataSamples.map(i => AirplaneProfileFile.fromBinary(b64UnzipBytes(i)))
+    timelineSamples.value = dataReady.dataSamples.map(i => TimelineFile.fromBinary(b64UnzipBytes(i)))
+    status.value = "ready"
   }
 }, { immediate: true })
 
 </script>
 
 <template>
-  <div v-if="pending" class="flex flex-col items-center text-white">
-    <ToolLoading message="Loading..." />
-  </div>
-  <div v-else-if="error" class="flex flex-col items-center text-white">
+  <div v-if="error" class="flex flex-col items-center text-white">
     Error: {{ error.message }}
   </div>
-  <div v-else-if="profile" class="flex flex-col items-center text-white">
-    <ProfilerCard v-if="screenStore.screen === 'profiler'" :flare="profile" class="animate-fade-left" />
-    <ConfigCard v-if="screenStore.screen === 'config'" :configs="profile.configs" class="animate-fade-left" />
+  <div v-else-if="status === 'pending'" class="flex flex-col items-center text-white">
+    <ToolLoading message="Loading..." />
+  </div>
+  <div v-else-if="status === 'ready'" class="flex flex-col items-center text-white">
+    <ProfilerCard v-if="screenStore.screen === 'profiler'" :flare="profile!" class="animate-fade-left" />
+    <ConfigCard v-if="screenStore.screen === 'config'" :configs="profile!.configs" class="animate-fade-left" />
     <!-- WorldCard -->
-    <SystemCard v-if="screenStore.screen === 'system'" :hwInfo="profile.hwinfo!" :os="profile.os!" :vmOptions="profile.vmoptions!" :v3="profile.v3!" class="animate-fade-left" />
+    <SystemCard v-if="screenStore.screen === 'system'" :hwInfo="profile!.hwinfo!" :os="profile!.os!" :vmOptions="profile!.vmoptions!" :v3="profile!.v3!" class="animate-fade-left" />
   </div>
 </template>
 
