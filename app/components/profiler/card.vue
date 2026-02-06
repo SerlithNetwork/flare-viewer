dcc04bc31a68d05644b07123133d29a24927cf8d
Biquaternions <biquaternions@serlith.net>
2026-02-06 13:57:24 -0500

    Use skeleton while loading threads

--- app/components/profiler/card.vue
+++ app/components/profiler/card.vue
@@ -23,7 +23,10 @@ const dictionary = computed(() => mergeDictionarySlices(airplaneData.value.dicti
           <UButton icon="i-lucide-memory-stick" size="xl" color="secondary" class="font-bold text-lg" @click="profilerType = 'memory'" :variant="profilerType === 'memory' ? 'solid' : 'outline'" >Memory</UButton>
         </div>
       </div>
-      <div class="flex flex-col items-start w-full rounded-lg" v-bind="$attrs">
+      <div v-if="airplaneData.timeProfile.length === 0 || airplaneData.memoryProfile.length === 0" class="flex flex-col gap-1" >
+        <USkeleton v-for="i in 25" :key="i" class="h-6 w-full" />
+      </div>
+      <div v-else class="flex flex-col items-start w-full rounded-lg" v-bind="$attrs">
         <ProfilerThread v-if="profilerType === 'cpu'" v-for="thread in airplaneData.timeProfile" :key="thread.thread" mode="cpu" :dictionary="dictionary" :timeProfile="thread" />
         <ProfilerThread v-if="profilerType === 'memory'" v-for="thread in airplaneData.memoryProfile" :key="thread.thread" mode="memory" :dictionary="dictionary" :memoryProfile="thread" />
       </div>
