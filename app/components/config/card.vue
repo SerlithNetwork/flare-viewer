b7bb9abedce51163ef94973dfe361dd7d695398d
Biquaternions <biquaternions@serlith.net>
2026-06-21 12:14:08 -0500

    Zed wanted to reformat the file

--- app/components/config/card.vue
+++ app/components/config/card.vue
@@ -1,36 +1,38 @@
 <script setup lang="ts">
+import "highlight.js/styles/nord.css";
+import { type CreateProfile_ConfigurationFile } from "~/proto/ProfileFile_pb";
+import type { TabsItem } from "#ui/components/Tabs.vue";
 
-import 'highlight.js/styles/nord.css';
-import {type CreateProfile_ConfigurationFile} from "~/proto/ProfileFile_pb";
-import type {TabsItem} from "#ui/components/Tabs.vue";
+const { configs } = defineProps<{
+  configs: CreateProfile_ConfigurationFile[];
+}>();
+const configMap = computed(
+  () => new Map(configs.map((config) => [config.filename, config.contents])),
+);
 
-const { configs } = defineProps<{ configs: CreateProfile_ConfigurationFile[] }>()
-const configMap = computed(() => new Map(configs.map(config => [config.filename, config.contents])))
-
-
-const selectedConfig = ref(configs[0]!.filename)
-const selectedContent = ref(configs[0]!.contents)
-const items = computed<TabsItem[]>(() => configs.map(config => ({
-  label: config.filename,
-  icon: "i-lucide-file-cog",
-  value: config.filename,
-})))
-
+const selectedConfig = ref(configs[0]!.filename);
+const selectedContent = ref(configs[0]!.contents);
+const items = computed<TabsItem[]>(() =>
+  configs.map((config) => ({
+    label: config.filename,
+    icon: "i-lucide-file-cog",
+    value: config.filename,
+  })),
+);
 
 const selected = computed<string>({
   get() {
-    return selectedConfig.value
+    return selectedConfig.value;
   },
   set(value) {
-    selectedContent.value = configMap.value.get(value)!
-    selectedConfig.value = value
+    selectedContent.value = configMap.value.get(value)!;
+    selectedConfig.value = value;
   },
-})
-
+});
 </script>
 
 <template>
-  <div class="flex flex-col items-center min-h-6 w-[90%] m-8" >
+  <div class="flex flex-col items-center min-h-6 w-[90%] m-8">
     <div class="flex flex-col w-full gap-4">
       <div class="flex justify-start items-center w-full">
         <UScrollArea orientation="horizontal">
@@ -38,14 +40,17 @@ const selected = computed<string>({
         </UScrollArea>
       </div>
       <UCard class="w-full">
-        <highlightjs class="text-wrap w-full" :code="selectedContent" :autodetect="true" />
+        <highlightjs
+          class="text-wrap w-full"
+          :code="selectedContent"
+          :autodetect="true"
+        />
       </UCard>
     </div>
   </div>
 </template>
 
 <style scoped>
-
 ::-webkit-scrollbar-track {
   border-radius: 4px;
   background-color: var(--ui-bg-accented);
@@ -64,5 +69,4 @@ const selected = computed<string>({
 ::-webkit-scrollbar-corner {
   display: none;
 }
-
 </style>
