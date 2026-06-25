45aeeca1f78666147146d9346ecf4b58a51ea125
Biquaternions <biquaternions@serlith.net>
2026-06-24 21:54:45 -0500

    Savepoint

--- app/types/timeline.d.ts
+++ app/types/timeline.d.ts
@@ -1,10 +1,20 @@
-
 export interface MetricInstant {
-    time: string;
-    value: number;
+  time: string;
+  value: number;
 }
 
 export interface LiveSummary {
-    events: Map<string, MetricInstant[]>;
-    metrics: Map<string, MetricInstant[]>;
+  events: Map<string, MetricInstant[]>;
+  metrics: Map<string, MetricInstant[]>;
+}
+
+export interface WorldMetrics {
+  world: string;
+  mspt: MetricInstant[];
+}
+
+export interface RegionMetrics {
+  region: string;
+  tps: MetricInstant[];
+  mspt: MetricInstant[];
 }
--- app/util/live-utils.ts
+++ app/util/live-utils.ts
@@ -15,6 +15,13 @@ export function groupTimelineSamples(samples: TimelineFile[]): LiveSummary {
       hour12: false,
     });
     for (const entry of sample.live) {
+      if (
+        entry.type.startsWith("flare:world") ||
+        entry.type.startsWith("flare:region")
+      ) {
+        continue;
+      }
+
       const avg = entry.data.reduce((a, b) => a + b, 0) / entry.data.length;
       if (!Number.isFinite(avg)) {
         continue;
@@ -62,4 +69,58 @@ export function groupTimelineSamples(samples: TimelineFile[]): LiveSummary {
   }
 
   return { events, metrics };
+}
+
+export function filterWorldAndRegionSamples(samples: TimelineFile[]) {
+  const worldMspt = new Map<string, Record<string, number>>();
+  const regionMspt = new Map<string, Record<string, number>>();
+  const regionTps = new Map<string, Record<string, number>>();
+
+  for (const sample of samples) {
+    const timeString = new Date(sample.startedAt).toLocaleString("en-US", {
+      day: "numeric",
+      month: "short",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: false,
+    });
+    for (const entry of sample.live) {
+      if (
+        !entry.type.startsWith("flare:world") &&
+        !entry.type.startsWith("flare:region")
+      ) {
+        continue;
+      }
+
+      const splits = entry.type.split(":");
+      if (splits.length !== 3) {
+        continue;
+      }
+
+      const tag = splits[1]!;
+      const match = tag.match(/\[(.*?)\]/);
+      if (!match) {
+        continue;
+      }
+
+      const identifier = match[1];
+      if (tag.startsWith("world")) {
+        if (entry.type.endsWith("mspt")) {
+        }
+      }
+      if (tag.startsWith("region")) {
+        if (entry.type.endsWith("tps")) {
+        }
+        if (entry.type.endsWith("mspt")) {
+        }
+      }
+    }
+  }
+
+  return {
+    worldMspt,
+    regionMspt,
+    regionTps,
+  };
 }
